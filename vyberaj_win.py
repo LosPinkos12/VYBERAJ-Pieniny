82e1e33b08a0ab9a09a13f9f517eaf575da04295
LosPinkos12 <90829696+LosPinkos12@users.noreply.github.com>
2024-06-23 12:49:00 +0200

    pywinusb

--- vyberaj_win.py
+++ vyberaj_win.py
@@ -1,13 +1,11 @@
 import os
 import shutil
 import time
-import win32file
-import win32api
-import win32con
 import threading
+import pywinusb.hid as hid
 
 # Adresár, kam sa majú kopírovať fotky
-destination_folder = r"C:\\k\priecinku"
+destination_folder = r"C:\cesta\k\priecinku"
 
 def copy_photos(source_dir, dest_dir):
     print("Copying photos from", source_dir, "to", dest_dir)
@@ -17,17 +15,18 @@ def copy_photos(source_dir, dest_dir):
             print("Copied", filename)
 
 def monitor_usb():
-    device_filter = win32file.HDEVINFO()
-    win32file.SetupDiGetClassDevs(None, 'USB', None, win32file.DIGCF_PRESENT | win32file.DIGCF_DEVICEINTERFACE)
-    while True:
-        win32file.SetupDiEnumDeviceInterfaces(device_filter, None, 'USB', None)
-        time.sleep(1)
+    # Funkcia na monitorovanie USB zariadení pomocou pywinusb
+    devices = hid.find_all_hid_devices()
 
-def device_event(device):
-    if 'Canon' in device:
-        print("Canon EOS R6 detected.")
-        source_dir = device
-        copy_photos(source_dir, destination_folder)
+    while True:
+        for device in devices:
+            if "Canon" in device.product_name:
+                # Získanie cesty k zariadeniu
+                source_dir = device.device_path
+                device.close()
+                copy_photos(source_dir, destination_folder)
+                return
+        time.sleep(1)
 
 def main():
     monitoring_thread = threading.Thread(target=monitor_usb)
@@ -38,7 +37,7 @@ def main():
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        monitoring_thread.join()
+        pass
 
 if __name__ == "__main__":
     main()
